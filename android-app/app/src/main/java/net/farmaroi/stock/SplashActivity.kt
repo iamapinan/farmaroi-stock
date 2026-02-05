6a3b117e29b6ff20610f3163a76b85c076906793
Apinan Woratrakun <iamapinan@gmail.com>
2026-02-05 22:44:07 +0700

    fix authen and add google method

---
+++ android-app/app/src/main/java/net/farmaroi/stock/SplashActivity.kt
@@ -0,0 +1,22 @@
+package net.farmaroi.stock
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    
+    private val splashTimeOut: Long = 2000 // 2 seconds
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Navigate to MainActivity after splash timeout
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeOut)
+    }
+}
